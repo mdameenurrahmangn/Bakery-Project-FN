6a996dfb9d9e37fb7f5581e0d8ccf0bdd7b07479
mdameenurrahmangn <mohammedameenurrahman43@gmail.com>
2024-10-27 19:08:28 +0530

    modified styles,1

--- src/component/OneGoal/OneGoal.jsx
+++ src/component/OneGoal/OneGoal.jsx
@@ -1,23 +1,21 @@
 
-
-
 const OneGoal = () => {
     return (
         <>
             <section className="py-2 bg-[#a79f94]">
                 <div class="2xl:container mx-auto">
                     <div className="w-[90%] mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 mt-48 mb-36">
-                        <div>
+                        <div className="">
                             <img className="rounded-2xl" src="https://assets.nicepagecdn.com/d2cc3eaa/3266805/images/tttt.jpg" alt="" />
                         </div>
                         
-                        <div className="flex flex-col justify-center items-start border-8 border-[#ff9800]  gap-4 m-4 bg-white p-6 rounded-2xl -mt-14 -ml-5 ">
+                        {/* <div className="flex flex-col justify-center items-start border-8 border-[#ff9800]  gap-4 m-4 bg-white p-6 rounded-2xl -mt-14 -ml-4 ">
                             <p className="text-black text-2xl font-bold">We've had one goal: to bake the best tasting breads, cakes and pastries.</p>
                             <p>After baking, bread usually presents some pleasant characteristics such as a brownish and crunchy crust, a pleasant aroma, a soft and elastic crumb texture, and a moist mouthfeel. Bread
                                 provides also essential dietary minerals, mostly magnesium,
                                 calcium and potassium</p>
                             <button class="text-black  font-bold text-base  p-3 w-36 border-2 border-black rounded-xl hover:bg-black hover:text-white">View product</button>
-                        </div>
+                        </div> */}
 
                         <div className="flex flex-col justify-center items-center -mr-11 -mt-11">
                                 <img className="h-48 rounded-2xl border-4 border-white" src="https://st2.depositphotos.com/1027198/8127/i/450/depositphotos_81270964-stock-photo-croissant-and-various-bread.jpg" alt="" />
