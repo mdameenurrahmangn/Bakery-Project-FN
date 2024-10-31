031c3ce3d0b0a11b6606aa74a61f3d3b3ccff54e
mdameenurrahmangn <mohammedameenurrahman43@gmail.com>
2024-10-31 16:05:41 +0530

    updated header

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -13,10 +13,16 @@ const Header = () => {
       <div className="w-[90%] text-white  text-xl mx-auto grid grid-cols-1 md:grid-cols-2 ">
         {/* logo */}
         <div className="flex justify-between">
+          <div className="flex flex-row justify-center items-center">
           <Link to="/">
-            <img className="h-20" src="https://ik.imagekit.io/pvrlbjuaj/MERN%20stack/Bakery%20Logo.png" alt="" />
-          </Link>
-          <div className="">
+              <img className="h-20 mr-1" src="https://ik.imagekit.io/pvrlbjuaj/MERN%20stack/Bakery%20Logo.png" alt="" />
+            </Link>
+            <p className="text-[#5e2614] lg:text-3xl sm:text-2xl font-bold">Premium Bakery</p>
+
+          </div>
+          {/* <div className="flex flex-col justify-center items-center ml-2 md:ml-0">
+          </div> */}
+          <div className="fle">
             {/* Menu Toggle Icon */}
             {show ? (
               <svg
@@ -47,23 +53,23 @@ const Header = () => {
         </div>
         {/* content */}
         <div className={show ? "block" : "hidden md:block"}>
-          <ul className="p-6 text-white font-bold text-xl flex flex-col justify-center items-center md:flex-row md:justify-end gap-4">
+          <ul className="p-4 lg:border-4 lg:bg-black border-rose-900 rounded-full text-white font-bold text-xl flex flex-col justify-center items-center md:flex-row md:justify-end gap-4">
             <li id="home">
-              <Link className="hover:text-black" to="/" onClick={toggle}>
+              <Link className="hover:text-white" to="/" onClick={toggle}>
                 {/* onClick - TO GET Auto Close Feature */}
                 Home
               </Link>
             </li>
-            <Link className="relative text-white hover:text-orange-950 cursor-pointer transition-all ease-in-out before:transition-[width] before:ease-in-out before:duration-700 before:absolute before:bg-white before:origin-center before:h-[2px] before:w-0 hover:before:w-[50%] before:bottom-0 before:left-[50%] after:transition-[width] after:ease-in-out after:duration-700 after:absolute after:bg-white after:origin-center after:h-[2px] after:w-0 hover:after:w-[50%] after:bottom-0 after:right-[50%]" to="/fresh" onClick={toggle}>
+            <Link className="relative text-white hover:text-orange-500 cursor-pointer transition-all ease-in-out before:transition-[width] before:ease-in-out before:duration-700 before:absolute before:bg-white before:origin-center before:h-[2px] before:w-0 hover:before:w-[50%] before:bottom-0 before:left-[50%] after:transition-[width] after:ease-in-out after:duration-700 after:absolute after:bg-white after:origin-center after:h-[2px] after:w-0 hover:after:w-[50%] after:bottom-0 after:right-[50%]" to="/fresh" onClick={toggle}>
               FreshBakes
             </Link>
-            <Link className="relative text-white hover:text-orange-950 cursor-pointer transition-all ease-in-out before:transition-[width] before:ease-in-out before:duration-700 before:absolute before:bg-white before:origin-center before:h-[2px] before:w-0 hover:before:w-[50%] before:bottom-0 before:left-[50%] after:transition-[width] after:ease-in-out after:duration-700 after:absolute after:bg-white after:origin-center after:h-[2px] after:w-0 hover:after:w-[50%] after:bottom-0 after:right-[50%]" to="/features1" onClick={toggle}>
+            <Link className="relative text-white hover:text-orange-500 cursor-pointer transition-all ease-in-out before:transition-[width] before:ease-in-out before:duration-700 before:absolute before:bg-white before:origin-center before:h-[2px] before:w-0 hover:before:w-[50%] before:bottom-0 before:left-[50%] after:transition-[width] after:ease-in-out after:duration-700 after:absolute after:bg-white after:origin-center after:h-[2px] after:w-0 hover:after:w-[50%] after:bottom-0 after:right-[50%]" to="/features1" onClick={toggle}>
               Features
             </Link>
-            <Link className="relative text-white hover:text-orange-950 cursor-pointer transition-all ease-in-out before:transition-[width] before:ease-in-out before:duration-700 before:absolute before:bg-white before:origin-center before:h-[2px] before:w-0 hover:before:w-[50%] before:bottom-0 before:left-[50%] after:transition-[width] after:ease-in-out after:duration-700 after:absolute after:bg-white after:origin-center after:h-[2px] after:w-0 hover:after:w-[50%] after:bottom-0 after:right-[50%]" to="/rating" onClick={toggle}>
+            <Link className="relative text-white hover:text-orange-500 cursor-pointer transition-all ease-in-out before:transition-[width] before:ease-in-out before:duration-700 before:absolute before:bg-white before:origin-center before:h-[2px] before:w-0 hover:before:w-[50%] before:bottom-0 before:left-[50%] after:transition-[width] after:ease-in-out after:duration-700 after:absolute after:bg-white after:origin-center after:h-[2px] after:w-0 hover:after:w-[50%] after:bottom-0 after:right-[50%]" to="/rating" onClick={toggle}>
               Ratings
             </Link>
-            <Link className="relative text-white hover:text-orange-950 cursor-pointer transition-all ease-in-out before:transition-[width] before:ease-in-out before:duration-700 before:absolute before:bg-white before:origin-center before:h-[2px] before:w-0 hover:before:w-[50%] before:bottom-0 before:left-[50%] after:transition-[width] after:ease-in-out after:duration-700 after:absolute after:bg-white after:origin-center after:h-[2px] after:w-0 hover:after:w-[50%] after:bottom-0 after:right-[50%]" to="/contact" onClick={toggle}>
+            <Link className="relative text-white hover:text-orange-500 cursor-pointer transition-all ease-in-out before:transition-[width] before:ease-in-out before:duration-700 before:absolute before:bg-white before:origin-center before:h-[2px] before:w-0 hover:before:w-[50%] before:bottom-0 before:left-[50%] after:transition-[width] after:ease-in-out after:duration-700 after:absolute after:bg-white after:origin-center after:h-[2px] after:w-0 hover:after:w-[50%] after:bottom-0 after:right-[50%]" to="/contact" onClick={toggle}>
               Contact
             </Link>
           </ul>
